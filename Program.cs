de2e0ed633b0f53f3736a9b32b83ecc54cb26d52
AleksaNikiforova <145163835+AleksaNikiforova@users.noreply.github.com>
2024-02-26 17:26:42 +0300

    create New Array

--- Program.cs
+++ Program.cs
@@ -10,3 +10,18 @@
     }
     return newIndex;
 }
+
+string[] createNewArray(int newIndex, string[] arr)
+{
+    string[] newFirstArray = new string[newIndex];
+    newIndex = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <= 3)
+        {
+            newFirstArray[newIndex] = arr[i];
+            newIndex +=1;
+        }
+    }
+    return newFirstArray;
+}
