ad446b5507d306745cdfb329ca7f8a89686a30d7
AleksaNikiforova <145163835+AleksaNikiforova@users.noreply.github.com>
2024-02-26 17:09:50 +0300

    Add files via upload
    get New Array Size

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿int getNewFirstArraySize(string[] arr)
+{
+    int newIndex = 0;
+    foreach (string s in arr)
+    {
+        if (s.Length <= 3)
+        {
+            newIndex += 1;
+        }
+    }
+    return newIndex;
+}
